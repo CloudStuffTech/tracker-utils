d9847947b43c04e6ab6ef9ce893b23cdf4e3013e
Himanshurao27 <Himanshurao@trackier.com>
2021-11-24 14:59:25 +0530

    feat-encrypt-decrypt-function

--- src/security.js
+++ src/security.js
@@ -49,6 +49,28 @@ class Security {
     }
     return ipaddr;
   }
+
+  encrypt (text, key) {
+    const passwordHash = crypto.createHash('sha256').update(key).digest("hex").substr(0, 32);
+    const iv = Buffer.from(crypto.randomBytes(16)).toString("hex").slice(0, 16);
+    const cipher = crypto.createCipheriv("AES-256-CBC", passwordHash, iv);
+    const cipherText = cipher.update(text, "utf8", "base64") + cipher.final("base64");
+    const encodedIv = Buffer.from(iv).toString('base64');
+    const hmac = crypto.createHmac("sha256",cipherText);
+    const hashHmac = hmac.update(Buffer.from(passwordHash, 'utf-8')).digest();
+    const encodedHmac = Buffer.from(hashHmac).toString('base64').substr(0,44);
+    return encodedIv+encodedHmac+cipherText;
+  }
+
+  decrypt (text, key) {
+    const passwordHash = crypto.createHash('sha256').update(key).digest("hex").substr(0, 32);
+    const decodedIv = text.substring(24,0);
+    const iv = Buffer.from(decodedIv,'base64'); 
+    const decipherText = text.slice(68);
+    const decipher = crypto.createDecipheriv("AES-256-CBC", passwordHash, iv);
+    const originalText = decipher.update(decipherText, "base64", "utf8") + decipher.final("utf8");
+    return originalText;
+  }
 }
 
 module.exports = new Security;
