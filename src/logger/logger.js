0afe9b953cc142b5ebeb1ce2c3c760ecc8b84f39
Hemant Mann <hemant.mann121@gmail.com>
2022-12-08 16:28:52 +0530

    Update logger.js

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -11,15 +11,6 @@ const logger = (() => {
 		format: winston.format.json(),
 		defaultMeta: { service: "pb-srvc" },
 		transports: [
-			//
-			// - Write all logs with importance level of `error` or less to `error.log`
-			// - Write all logs with importance level of `info` or less to `combined.log`
-			//
-			// new winston.transports.File({
-			// 	filename: "error.log",
-			// 	level: "error",
-			// 	format: winston.format.printf((info) => getLogstring(info)),
-			// }),
 			new winston.transports.File({ filename: "combined.log", format: winston.format.printf((info) => getLogstring(info)) }),
 		],
 	});
