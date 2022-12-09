e0b826a4a26f5a5c16b2dfb8efcb0bbb0520ebed
vivek-kumar-vkrs <107498771+vivek-kumar-vkrs@users.noreply.github.com>
2022-12-09 10:23:41 +0530

    Add basic config support in logr

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,60 +1,67 @@
 const winston = require("winston");
 
 const logger = (() => {
-	const LOGGER_ENABLED = true;
-	let requestId = "sys_default";
-	let userId = "sys_default";
-	let user = "sys_default";
+    let LOGGER_ENABLED = true;
+    let requestId = "sys_default";
+    let userId = "sys_default";
+    let user = "sys_default";
+    let logr;
 
-	const logr = winston.createLogger({
-		level: "info",
-		format: winston.format.json(),
-		defaultMeta: { service: "pb-srvc" },
-		transports: [
-			new winston.transports.File({ filename: "combined.log", format: winston.format.printf((info) => getLogstring(info)) }),
-		],
-	});
+    const init = ({ service, enableConsoleLog = false, loggerEnabled = true }) => {
+        if (!loggerEnabled) LOGGER_ENABLED = false;
+        logr = winston.createLogger({
+            level: "info",
+            format: winston.format.json(),
+            defaultMeta: { service },
+            transports: [
+                new winston.transports.File({
+                    filename: "combined.log",
+                    format: winston.format.printf((info) => getLogstring(info)),
+                }),
+            ],
+        });
+        // Add log on console in case of dev environment
+        if (enableConsoleLog) {
+            logr.add(
+                new winston.transports.Console({
+                    format: winston.format.printf((info) => getLogstring(info)),
+                })
+            );
+        }
+    };
 
-	if (process.env.NODE_ENV !== "production") {
-		logr.add(
-			new winston.transports.Console({
-				// format: winston.format.simple(),
-				format: winston.format.printf((info) => getLogstring(info)),
-			})
-		);
-	}
+    const updateLogData = (data) => {
+        if (data["requestId"]) requestId = data["requestId"];
+        if (data["userId"]) userId = data["userId"];
+        if (data["user"]) userType = data["user"];
+    };
 
-	const updateLogData = (data) => {
-		if (data["requestId"]) requestId = data["requestId"];
-		if (data["userId"]) userId = data["userId"];
-		if (data["user"]) userType = data["user"];
-	};
+    const getLogstring = (info) => {
+        return `[REQ_ID: ${requestId}][USER: ${user}][USER_ID: ${userId}][LEVEL:${info.level}][MSG:${info.message}]`;
+    };
 
-	const getLogstring = (info) => {
-		return `[REQ_ID: ${requestId}][USER: ${user}][USER_ID: ${userId}][LEVEL:${info.level}][MSG:${info.message}]`;
-	};
+    const info = (msg) => {
+        if (!LOGGER_ENABLED || !logr) return;
+        logr.info(msg);
+    };
 
-	const info = (msg) => {
-		if (!LOGGER_ENABLED) return;
-		logr.info(msg);
-	};
+    const error = (msg) => {
+        if (!LOGGER_ENABLED || !logr) return;
+        logr.error(msg);
+    };
 
-	const error = (msg) => {
-		if (!LOGGER_ENABLED) return;
-		logr.error(msg);
-	};
+    const warn = (msg) => {
+        if (!LOGGER_ENABLED || !logr) return;
+        logr.warn(msg);
+    };
 
-	const warn = (msg) => {
-		if (!LOGGER_ENABLED) return;
-		logr.warn(msg);
-	};
-
-	return {
-		info,
-		error,
-		warn,
-		updateLogData,
-	};
+    return {
+        info,
+        error,
+        warn,
+        updateLogData,
+        init,
+    };
 })();
 
 module.exports = { logger };
