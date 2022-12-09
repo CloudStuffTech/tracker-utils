831b56836aface165324b3f3ab19808108dee0ca
vivek-kumar-vkrs <107498771+vivek-kumar-vkrs@users.noreply.github.com>
2022-12-09 21:53:48 +0530

    Update requestv2

--- src/requestV2/got.js
+++ src/requestV2/got.js
@@ -4,11 +4,12 @@ const request = (() => {
     const get = async ({ endpoint, retry = 3 }) => {
         try {
             const options = { retry: { limit: retry } };
+            const start = Date.now();
             const data = await got.get(endpoint, options);
 
-            return { data, err: null };
+            return { data, err: null, latency: start - Date.now() };
         } catch (err) {
-            return { data: null, err };
+            return { data: null, err, latency: null };
         }
     };
     return {
