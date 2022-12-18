ddb1c21e4b76fde951f3e080b07d1770dbaea67f
vivek-kumar-vkrs <107498771+vivek-kumar-vkrs@users.noreply.github.com>
2022-12-18 11:32:46 +0530

    Add sleep in tools

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,16 +1,28 @@
 const tools = (() => {
-    handleShutdown = ({ params, handleInterrupt }) => {
-        // Quit from keyboard ctl + c
-        process.on("SIGINT", () => handleInterrupt("SIGINT", params));
-        // Quit from keyboard
-        process.on("SIGQUIT", () => handleInterrupt("SIGQUIT", params));
-        // quit by operating system kill command
-        process.on("SIGTERM", () => handleInterrupt("SIGTERM", params));
-    };
+	handleShutdown = ({ params, handleInterrupt }) => {
+		// Quit from keyboard ctl + c
+		process.on("SIGINT", () => handleInterrupt("SIGINT", params));
+		// Quit from keyboard
+		process.on("SIGQUIT", () => handleInterrupt("SIGQUIT", params));
+		// quit by operating system kill command
+		process.on("SIGTERM", () => handleInterrupt("SIGTERM", params));
+	};
 
-    return {
-        handleShutdown,
-    };
+	/**
+	 * Call this function to sleep for a given milliseconds
+	 * @param  {Number} ms time in milliseconds
+	 * @return {Promise}
+	 */
+	function sleep(ms) {
+		return new Promise(resolve => {
+			setTimeout(() => resolve(true), ms);
+		})
+	}
+
+	return {
+		handleShutdown,
+		sleep
+	};
 })();
 
 module.exports = { tools };
