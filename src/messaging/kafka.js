e49d5fbbe164b9f2a111ec9677a28d4dc1046321
vivek-kumar-vkrs <107498771+vivek-kumar-vkrs@users.noreply.github.com>
2022-12-27 15:25:55 +0530

    Update kafka wrapper

--- src/messaging/kafka.js
+++ src/messaging/kafka.js
@@ -5,6 +5,7 @@ const os = require("os");
 const kafka = () => {
 	let kafkaClient = null;
 	let producer = null;
+	let consumer = null;
 
 	const init = (config) => {
 		// Initializing Kafka Client
@@ -62,7 +63,78 @@ const kafka = () => {
 		await producer.send({ topic, messages, });
 	}
 
-	return { init, createProducer, connectProducer, disconnectProducer, send }
+	const createConsumer = ({ groupId }) => {
+		if (consumer) {
+			return false
+		}
+		consumer = kafkaClient.consumer({ groupId });
+	}
+	const connectConsumer = async () => {
+		await consumer.connect();
+
+	}
+
+	const subscribeConsumer = async ({ topic, fromBeginning = false, }) => {
+		if (!consumer) {
+			return false
+		}
+		await consumer.subscribe({ topic, fromBeginning, })
+	}
+
+	const handlConsumerMsg = async ({ eachMessageHandler, batchMessageHandler, eachBatchAutoResolve = false }) => {
+		if (eachMessageHandler) {
+			await consumer.run({
+				eachMessage: eachMessageHandler,
+			})
+		} else {
+			await consumer.run({
+				eachBatchAutoResolve,
+				eachBatch: batchMessageHandler,
+			})
+		}
+	}
+
+	const handleConmsumerShutdown = ({
+		signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'],
+		errorTypes = ['unhandledRejection', 'uncaughtException'],
+
+		disconnectOnInterupt = true,
+		disconnectOnError = true,
+
+	} = {}) => {
+
+		if (disconnectOnInterupt) {
+			errorTypes.forEach(type => {
+				process.on(type, async e => {
+					try {
+						console.log(`process.on ${type}`)
+						console.error(e)
+						await consumer.disconnect()
+						process.exit(0)
+					} catch (_) {
+						process.exit(1)
+					}
+				})
+			})
+		}
+
+		if (disconnectOnError) {
+			signalTraps.forEach(type => {
+				process.once(type, async () => {
+					try {
+						await consumer.disconnect()
+					} finally {
+						process.kill(process.pid, type)
+					}
+				})
+			})
+		}
+	}
+
+	return {
+		init, createProducer, connectProducer, disconnectProducer, send, createConsumer,
+		connectConsumer, subscribeConsumer, handlConsumerMsg, handleConmsumerShutdown
+	}
 }
 
 module.exports = { kafka }
