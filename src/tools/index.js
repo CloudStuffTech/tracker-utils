71b4135f83326fe0480b1030340bdd1572829301
Hemant Mann <hemant.mann121@gmail.com>
2024-04-17 22:47:20 +0530

    update: tool.js

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,28 +1,21 @@
-const tools = (() => {
-	handleShutdown = ({ params, handleInterrupt }) => {
-		// Quit from keyboard ctl + c
-		process.on("SIGINT", () => handleInterrupt("SIGINT", params));
-		// Quit from keyboard
-		process.on("SIGQUIT", () => handleInterrupt("SIGQUIT", params));
-		// quit by operating system kill command
-		process.on("SIGTERM", () => handleInterrupt("SIGTERM", params));
-	};
+const handleShutdown = ({ params, handleInterrupt }) => {
+	// Quit from keyboard ctl + c
+	process.on("SIGINT", () => handleInterrupt("SIGINT", params));
+	// Quit from keyboard
+	process.on("SIGQUIT", () => handleInterrupt("SIGQUIT", params));
+	// quit by operating system kill command
+	process.on("SIGTERM", () => handleInterrupt("SIGTERM", params));
+};
 
-	/**
-	 * Call this function to sleep for a given milliseconds
-	 * @param  {Number} ms time in milliseconds
-	 * @return {Promise}
-	 */
-	function sleep(ms) {
-		return new Promise(resolve => {
-			setTimeout(() => resolve(true), ms);
-		})
-	}
+/**
+ * Call this function to sleep for a given milliseconds
+ * @param  {Number} ms time in milliseconds
+ * @return {Promise}
+ */
+const sleep = async (ms) => {
+	return new Promise(resolve => {
+		setTimeout(() => resolve(true), ms);
+	})
+}
 
-	return {
-		handleShutdown,
-		sleep
-	};
-})();
-
-module.exports = { tools };
+module.exports = { handleShutdown, sleep }
