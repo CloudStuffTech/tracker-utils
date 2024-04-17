cf206543b15a375ab141545ec9659d2f9d140a01
Hemant Mann <hemant.mann121@gmail.com>
2024-04-17 22:57:36 +0530

    update: factory class method doc

--- src/messaging/factory.js
+++ src/messaging/factory.js
@@ -1,6 +1,12 @@
 let PubSub = require('./pubsub');
 
 class Factory {
+	/**
+	 * This method will return the PubSub class object
+	 * @param {String} messagingType Message Type variable
+	 * @param {Object} config Config object
+	 * @returns {PubSub}
+	 */
 	make(messagingType, config) {
 		switch (messagingType) {
 			case "pubsub":
