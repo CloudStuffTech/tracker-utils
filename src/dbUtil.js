e548473b0deed9fb2d02377e647574b8931cf83b
Himanshurao27 <work.himanshu.rao@gmail.com>
2025-06-12 18:08:24 +0530

    Update dbUtil.js

--- src/dbUtil.js
+++ src/dbUtil.js
@@ -17,7 +17,10 @@ class DbUtil {
 
 	createConnection(config) {
 		let str = this.connectionStr(config);
-		return mongoose.createConnection(str, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, poolSize: config.poolSize || 5 });
+		return mongoose.createConnection(str, { 
+			useNewUrlParser: true, 
+			useUnifiedTopology: true
+		});
 	}
 
 	generateMongoId() {
